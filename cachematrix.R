cfa730ec61855923b8946b24625707f06c1e6bc5
Олексій Нєвєдров <allex.nevedrov@gmail.com>
2023-03-05 00:50:24 +0200

    File completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Implements inverse matrix memoization 
 
-## Write a short comment describing this function
+# Creates cache wrapper for inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse_matrix_cache = NULL
+    set_matrix<-function(new_matrix){
+        x<<-new_matrix
+        inverse_matrix_cache<<-NULL
+    }
+    get_matrix<-function(){
+        x
+    }
+    set_inverse<-function(new_inverse_matrix){
+        inverse_matrix_cache<<-new_inverse_matrix
+    }
+    get_inverse<-function(){
+        inverse_matrix_cache
+    }
+    list(set=set_matrix, get=get_matrix, set_cache=set_inverse, get_cache=get_inverse)
 }
 
 
-## Write a short comment describing this function
+# Gets inverse matrix from cache wrapper
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv_matrix<-x$get_cache()
+    if(!is.null(inv_matrix)){
+        print("cached")
+    }
+    else{
+        print("not cached")
+        inv_matrix<-solve(x$get())
+        x$set_cache(inv_matrix)
+    }
+    inv_matrix 
 }
